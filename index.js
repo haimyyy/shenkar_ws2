f6b1921ac66391a37b189cd26a76f701965a6ac8
haimyyy <haimyyy@gmail.com>
2015-05-21 14:36:31 +0300

    aa

--- index.js
+++ index.js
@@ -3,7 +3,11 @@ var url = require('url');
 var ScoreTable =require('./socredTable');
 app = express();
 
-
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", '*');
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
 /******* DB and init ********/
 
 var Player1={
